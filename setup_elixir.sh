cd362212497b61b0da83125415f009ecc293b60c
Nazar8 <118745073+88n77@users.noreply.github.com>
2024-10-20 08:30:19 +0000

    setup_elixir.sh

--- setup_elixir.sh
+++ setup_elixir.sh
@@ -24,7 +24,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
-newgrp docker
+
+echo "newgrp docker"
 
 echo -e "${GREEN}${BOLD}====================================================${RESET}"
 echo -e "${GREEN}${BOLD}Створення директорії для валідатора...${RESET}"
