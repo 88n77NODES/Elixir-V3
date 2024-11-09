2480dc3ba159162a244f05d805393ebd023c7df2
Nazar8 <118745073+88n77@users.noreply.github.com>
2024-11-09 17:02:43 +0000

    upd

--- Update_elixir.sh
+++ Update_elixir.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-set -e  # Завершити скрипт при будь-якій помилці
+set -e  # 
 
 GREEN='\e[38;5;46m' 
 YELLOW='\e[38;5;226m' 
 RESET='\e[0m'
 
-# 1. Перехід до директорії elixir
 echo -e "${GREEN}====================================================${RESET}"
 echo -e "${GREEN}Перехід до директорії elixir...${RESET}"
-cd elixir || { echo -e "${YELLOW}Директорія elixir не знайдена${RESET}"; exit 1; }
+cd elixir || { echo -e "${YELLOW}Директорія elixir не знайдена, вихід.${RESET}"; exit 1; }
 
 echo -e "${GREEN}Видалення старих контейнерів elixir...${RESET}"
-docker kill elixir && \ echo -e "${YELLOW}Контейнерів не знайдено${RESET}"
-docker rm elixir && \
+docker kill elixir || echo -e "${YELLOW}Контейнер elixir не запущений, пропускаємо kill.${RESET}"
+docker rm elixir || echo -e "${YELLOW}Контейнер elixir не знайдено для видалення.${RESET}"
 
 echo -e "${GREEN}Встановлюємо оновлення...${RESET}"
-docker pull elixirprotocol/validator:testnet --platform linux/amd64 && \
+docker pull elixirprotocol/validator:testnet --platform linux/amd64
 
 echo -e "${GREEN}====================================================${RESET}"
-echo -e "${GREEN}Ноду оновлено!Запуск..${RESET}"
-
-echo -e "${GREEN}Запускаємо новий контейнер...${RESET}"
+echo -e "${GREEN}Ноду оновлено! Запускаємо новий контейнер...${RESET}"
 docker run --env-file /root/elixir/validator.env --platform linux/amd64 --name elixir -p 17690:17690 elixirprotocol/validator:testnet
-
-
